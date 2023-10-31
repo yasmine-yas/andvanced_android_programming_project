610a9fc8aa5d2c5bc85ab72272afbfb09d85baae
yasmine muhamed <74177304+yasmine-yas@users.noreply.github.com>
2023-10-31 15:09:42 +0200

    Create ElectionsViewModelFactory.kt

---
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsViewModelFactory.kt
@@ -0,0 +1,18 @@
+package com.example.android.politicalpreparedness.election
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.android.politicalpreparedness.network.models.Election
+
+class ElectionsViewModelFactory(
+    private val app: Application
+): ViewModelProvider.Factory {
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ElectionsViewModel::class.java)) {
+            return ElectionsViewModel(app) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
